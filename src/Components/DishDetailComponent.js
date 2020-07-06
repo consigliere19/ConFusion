19dd8d8e5558b2c47a0e8e72387ff4dc3df63b7c
Bitanu <bitanu.ch@gmail.com>
2020-07-06 21:46:18 +0530

    Functional Components

--- src/Components/DishDetailComponent.js
+++ src/Components/DishDetailComponent.js
@@ -36,7 +36,7 @@ class DishDetail extends Component {
                         <div key={comm.id}>
                             <ul className="list-unstyled">
                                 <li>{comm.comment}</li>
-                                <li>-- {comm.author}, {months[date.getMonth()]} {date.getDate()}, {date.getFullYear()}</li>
+                                <li>-- {comm.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comm.date)))}</li>
                             </ul>
                         </div>
                     )
@@ -54,7 +54,7 @@ class DishDetail extends Component {
     render(){
         console.log(this.props.selectedDish)
         return(
-            
+            <div className="container">
             <div className="row">
               <div  className="col-12 col-md-5 m-1">
                 {this.renderDish(this.props.selectedDish)}
@@ -63,7 +63,7 @@ class DishDetail extends Component {
                 {this.renderComments(this.props.selectedDish)}
               </div>
             </div>
-       
+            </div>
         )
     }
 }
